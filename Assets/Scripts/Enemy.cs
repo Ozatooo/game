b3e0d114c1faa4fd6f0e2acfc0d9722ebc8ebbcf
Tobiugi <Tobiugi@gmail.com>
2024-04-19 13:19:34 +0200

    Chasing&EnemySpawne

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,6 +4,40 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    [SerializeField] float moveSpeed = 5f;
+    Rigidbody2D rb;
+    Transform target;
+    Vector2 moveDirection;
+    private EnemySpawner enemySpawner;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        target = GameObject.Find("Player").transform;
+        enemySpawner = FindObjectOfType<EnemySpawner>();
+    }
+
+    private void Update()
+    {
+        if (target)
+        {
+            Vector3 direction = (target.position - transform.position).normalized;
+            float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+            moveDirection = direction;
+        }
+    }
+    private void FixedUpdate()
+    {
+        if (target)
+        {
+            rb.velocity = new Vector2(moveDirection.x, moveDirection.y) * moveSpeed;
+        }
+    }
+
     public float Health
     {
         set
@@ -26,6 +60,10 @@ public class Enemy : MonoBehaviour
     public void Defeated()
     {
         DropLoot();
+        if (enemySpawner != null)
+        {
+            enemySpawner.EnemyDefeated();
+        }
         Destroy(gameObject);
     }
     public void DropLoot()
